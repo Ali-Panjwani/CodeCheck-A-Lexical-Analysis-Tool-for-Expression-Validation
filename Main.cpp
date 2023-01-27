6d823f8b207c5ef287db269342b732b7a658f692
Ali Panjwani <106570904+Ali-Panjwani@users.noreply.github.com>
2023-01-27 23:17:41 +0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include"ParseTreeAndSyntaxTree.h"
+#include"File_Checker.h"
+using namespace std;
+
+int main()
+{
+	StartBracesChecker();
+	EndBracesChecker();
+
+	int k;
+	fstream fp;
+	fp.open("input.txt",ios::out);
+	if (!fp)
+		cout << "Not Found..!";
+		A:
+		for ( k = position; instructions[k] != '$'; k++)
+		{
+			fp << instructions[k];
+		}
+		
+		fp.close();
+
+	Lex_Analysis L;
+	L.Lex_Analysis_Main();
+
+    CFG cfg;
+ 
+ 	int x = cfg.CheckALL();
+	if(x)
+	{
+		cout << "\nInstruction Compiled Succesfully..!"<<endl;
+		TopParseTree UP;
+		UP.InsertChild();
+		UP.preOrder();
+	 	input[100] = {'\0'};
+		token_size = 0;
+		position = k;
+		goto A; 
+
+	}
+	else
+	{
+		cout << "Syntax Error in Code"<<endl;
+		exit(0);
+ 	}
+	
+  
+}
+
+ 
